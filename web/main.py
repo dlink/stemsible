560cfaffd3e8b2f2549b861144e65bc2f3a24712
David Link <dvlink@gmail.com>
2015-06-26 18:52:22 -0400

    styling adjustments

--- web/main.py
+++ web/main.py
@@ -10,7 +10,7 @@ from messages import Messages
 class Main(HtmlPage):
 
     def __init__(self):
-        HtmlPage.__init__(self, 'Stemsible')
+        HtmlPage.__init__(self, 'Stemsible', include_form_tag=0)
         self.style_sheets = ['bootstrap/css/bootstrap.min.css',
                              'css/app.css']
         self.messages = Messages()
@@ -29,7 +29,9 @@ class Main(HtmlPage):
         return open('header-section.html', 'r').read()
 
     def _getBody(self):
-        return open('body-section.html', 'r').read() % self._getMessages()
+        return open('body-section.html', 'r').read() % (
+            self._getNewMessageCard() +
+            self._getMessages())
 
     def _getMessages(self):
         messages = self.messages.getMessages()['messages']
@@ -38,10 +40,14 @@ class Main(HtmlPage):
             o += self._getMessageCard(m)
         return div(o, id='messageArea')
 
+    def _getNewMessageCard(self):
+        return open('new-message.html', 'r').read()
+
     def _getMessageCard(self, message):
         message = odict(message)
-        user_icon = div(img(src='images/generic_icon.png'),
-                        class_='img-thumbnail')
+        user_icon = div(img(src='images/generic_icon.png',
+                            class_='img-thumbnail'),
+                        class_='userIcon')
         username = div(message.author,  class_='messageAuthor')
         date     = div(message.created, class_='messageDate')
         username_and_date = div(username + date,
@@ -49,12 +55,12 @@ class Main(HtmlPage):
         buttons = 'Like | Comment'
 
         o = ''
-        o += div(user_icon + username_and_date)
+        o += user_icon + username_and_date
         o += div(message.text, class_='messageText')
         #o += hr()
         #o += div(buttons, class_='messageButtons')
 
-        return div(o, class_='messageCard')
+        return div(o, class_='messageCard', id='message_card_%s' % message.id)
 
     def _getFooter(self):
         items = ['FAQ', 'About', 'Terms & Privacy', 'Contact']
