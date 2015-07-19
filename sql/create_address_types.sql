2e2b2133cdf0cdbba360bfd76a0c652c289a4eff
David Link <dvlink@gmail.com>
2015-07-18 22:15:26 -0400

    Data Model: adding static data to address_types and school_relationships

--- sql/create_address_types.sql
+++ sql/create_address_types.sql
@@ -6,6 +6,7 @@ create table address_types (
   id                 integer unsigned not null auto_increment primary key,
   code               varchar(20)      not null,
   name               varchar(255)     not null,
+  description        varchar(255)     ,
   active             boolean          not null,
   created            datetime         not null ,
   last_updated       timestamp        not null 
@@ -19,8 +20,7 @@ show warnings;
 set foreign_key_checks = 1;
 
 create trigger address_type_create before insert on address_types
-   for each row set new.created = now()
-;
+   for each row set new.created = now();
 
 load data local infile 'data/address_types.csv' into table address_types
 fields terminated by ',' optionally enclosed by '"' ignore 1 lines;
--- sql/create_school_relationships.sql
+++ sql/create_school_relationships.sql
@@ -6,6 +6,7 @@ create table school_relationships (
   id                 integer unsigned not null auto_increment primary key,
   code               varchar(20)      not null,
   name               varchar(255)     not null,
+  description        varchar(255)     ,
   active             boolean          not null,
   created            datetime         not null ,
   last_updated       timestamp        not null 
@@ -20,6 +21,10 @@ set foreign_key_checks = 1;
 
 create trigger school_relationsihps_create before insert
    on school_relationships
-   for each row set new.created = now()
-;
+   for each row set new.created = now();
+
+load data local infile 'data/school_relationships.csv'
+into table school_relationships
+fields terminated by ',' optionally enclosed by '"' ignore 1 lines;
+
 desc school_relationships;
