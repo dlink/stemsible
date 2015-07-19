097cc859cb7849c5db1a8dda77271a0d88d594d4
David Link <dvlink@gmail.com>
2015-07-18 22:22:48 -0400

    moved test data into it's own subdir

--- sql/load_test_data.sql
+++ sql/load_test_data.sql
@@ -1,13 +1,13 @@
 -- Load test users
 
-load data local infile 'data/test_users.csv' into table users
+load data local infile 'test_data/users.csv' into table users
 fields terminated by ',' optionally enclosed by '"' ignore 1 lines;
 
 select * from users;
 
 -- Load test messages
 
-load data local infile 'data/test_messages.csv' into table messages
+load data local infile 'test_data/messages.csv' into table messages
 fields terminated by ',' optionally enclosed by '"' ignore 1 lines;
 
 select * from messages;
