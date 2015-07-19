091730c679cc9f40caf9eb3e80f22068969f60c8
David Link <dvlink@gmail.com>
2015-07-18 22:32:28 -0400

    adding more test data

--- sql/build_all.sql
+++ sql/build_all.sql
@@ -7,17 +7,19 @@ drop table if exists user_schools;
 drop table if exists users;
 drop table if exists schools;
 drop table if exists school_districts;
-drop table if exists school_relationships;
 drop table if exists addresses;
+
+drop table if exists school_relationships;
 drop table if exists address_types;
 
 -- Create all tables
 
 source create_address_types.sql
-source create_addresses.sql
 source create_school_relationships.sql
-source create_schools.sql
+
+source create_addresses.sql
 source create_school_districts.sql
+source create_schools.sql
 source create_users.sql;
 source create_user_schools.sql
 source create_messages.sql;
--- sql/load_test_data.sql
+++ sql/load_test_data.sql
@@ -1,14 +1,31 @@
--- Load test users
+-- Load test addresses
+load data local infile 'test_data/addresses.csv' into table addresses
+fields terminated by ',' optionally enclosed by '"' ignore 1 lines;
+select * from addresses;
 
+-- Load test school_districts
+load data local infile 'test_data/school_districts.csv'
+into table school_districts
+fields terminated by ',' optionally enclosed by '"' ignore 1 lines;
+select * from school_districts;
+
+-- Load test schools
+load data local infile 'test_data/schools.csv' into table schools
+fields terminated by ',' optionally enclosed by '"' ignore 1 lines;
+select * from schools;
+
+-- Load test users
 load data local infile 'test_data/users.csv' into table users
 fields terminated by ',' optionally enclosed by '"' ignore 1 lines;
-
 select * from users;
 
--- Load test messages
+-- Load test user_schools
+load data local infile 'test_data/user_schools.csv' into table user_schools
+fields terminated by ',' optionally enclosed by '"' ignore 1 lines;
+select * from user_schools;
 
+-- Load test messages
 load data local infile 'test_data/messages.csv' into table messages
 fields terminated by ',' optionally enclosed by '"' ignore 1 lines;
-
 select * from messages;
 
