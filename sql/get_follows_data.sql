9e113ce8b9b89f2371942d56b83f0c4b2379ad33
David Link <dvlink@gmail.com>
2015-07-24 17:30:08 -0400

    get_follows_data: updated column names

--- sql/get_follows_data.sql
+++ sql/get_follows_data.sql
@@ -1,5 +1,11 @@
 select
-   u1.username, u2.username, f.choice, f.grade, f.school, f.school_district
+   u1.username as follower,
+   u2.username as follwed,
+   f.choice,
+   f.grade,
+   f.school,
+   f.school_district,
+   f.active
 from
    follows f
    join users u1 on f.user_id = u1.id
