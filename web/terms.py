91cefe7d965b2de018cfc24d520db7538849e4e2
David Link <dvlink@gmail.com>
2016-08-07 23:10:50 +0000

    Terms and conditions

--- web/terms.py
+++ web/terms.py
@@ -7,21 +7,13 @@ from base import Base
 class Terms(Base):
 
     @property
-    def name(self): return 'terms'
+    def name(self): return 'about'
 
     def __init__(self):
         Base.__init__(self)
-        self.style = '''
-.text {
-   margin: 30px 100px;
-}
-'''
 
     def _getBody(self):
-        text = \
-            h2('Terms and Conditions') + \
-            p('Yet to be determined.')
-        return div(text, class_='text')
+        return open('terms.html', 'r').read()
 
 if __name__ == '__main__':
     Terms().go()
