257d69d464ca3272e9a656785eb515647053c9d5
David Link <dvlink@gmail.com>
2016-08-07 03:51:34 +0000

    about page: new copy and new image

--- web/about.py
+++ web/about.py
@@ -11,20 +11,9 @@ class About(Base):
 
     def __init__(self):
         Base.__init__(self)
-        self.style = '''
-.text {
-   margin: 30px 100px;
-}
-'''
 
     def _getBody(self):
-        text = \
-            h3('About') +\
-            p('Uday Kumar is an entrepreneurs living and working in the D.C. area, and has being working in the online education space for many years.') +\
-            p('While many social media platforms exist, Uday recognized that none existed for the parents of children in school.   That was the inspiration for Stemsible') +\
-            p('When Uday approached David Link, a computer engineer who also has worked in the educational space, he immediately liked the idea and started to build it - and Stemsible was created')
-        
-        return div(text, class_='text')
+        return open('about.html', 'r').read()
 
 if __name__ == '__main__':
     About().go()
