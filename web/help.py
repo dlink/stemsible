279ebf368d1b6b9160308854be6e35876a6ac66d
David Link <dvlink@gmail.com>
2016-08-07 22:55:27 +0000

    fleshed out HELP page

--- web/help.py
+++ web/help.py
@@ -7,21 +7,13 @@ from base import Base
 class Help(Base):
 
     @property
-    def name(self): return 'help'
+    def name(self): return 'about'
 
     def __init__(self):
         Base.__init__(self)
-        self.style = '''
-.text {
-   margin: 30px 100px;
-}
-'''
 
     def _getBody(self):
-        text = \
-            h3('Help') +\
-            p('Soon to come')
-        return div(text, class_='text')
+        return open('help.html', 'r').read()
 
 if __name__ == '__main__':
     Help().go()
