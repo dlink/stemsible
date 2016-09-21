ebde7bd49ee3f90db4d1c5971003d697f7a77912
Ignat Shining <mail@igonato.com>
2016-09-21 13:49:15 +0300

    Submit logic

--- web/js/url-preview.js
+++ web/js/url-preview.js
@@ -4,6 +4,13 @@ $(document).ready(function(){
 
     // Embedly preview plugin
     input.preview({key:'339bba310808442c8479798d9d44126f'});
+    form.on('submit', function(e){
+        var data = input.data('preview');
+        console.log(data);
+        $(this).addInputs(data);
+        return true;
+    });
+
 
     // For testing
     input.val('https://vimeo.com/182906334').blur();
